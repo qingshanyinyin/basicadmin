0c25f065e7b2737222ca12f9f7e15762379cc61e
zhangdian <vipzhangdian@gmail.com>
2017-09-07 16:02:45 +0800

    init

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=basetest',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '123',
     'charset' => 'utf8',
